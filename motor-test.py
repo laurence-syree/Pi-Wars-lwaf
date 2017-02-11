96164f96d8ef4e14a9df58d71a10b2002c56e078
Laurence Syree <laurence@syree.eu>
2017-02-11 10:08:17 +0000

    added pwm

--- motor-test.py
+++ motor-test.py
@@ -1,72 +1,94 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-direction = "l"
-distance = 4
+direction = ""
+while direction != "exit":
+	direction = raw_input("Please enter the direction: ")
+	distance = raw_input("Please enter the distance: ")
+	speed = float(raw_input("Please enter the speed (25-90): "))
 
-pins = {
-	"frf" : 33,
-	"frb" : 22,
-	"flf" : 35,
-	"flb" : 37,
-	"brf" : 32,
-	"brb" : 36,
-	"blf" : 38,
-	"blb" : 40
-}
+	# direction = "r"
+	# distance = 0.9
+	# speed = 90
 
-GPIO.setmode(GPIO.BOARD)
-# Setup the GPIO pins ready for control
-for key, pin in pins.items():
-	GPIO.setup(pin,GPIO.OUT)
+	pins = {
+		"frf" : 33,
+		"frb" : 22,
+		"flf" : 35,
+		"flb" : 37,
+		"brf" : 32,
+		"brb" : 36,
+		"blf" : 38,
+		"blb" : 40
+	}
 
-# Based on direction set the required direction pin to high
-if direction == "f":
-	# GPIO.setup(22, GPIO.OUT)# set GPIO 25 as an output. You can use any GPIO port they need  
-	# p = GPIO.PWM(22, 50)    # create an object p for PWM on port 25 at 50 Hertz  
-	#                         # you can have more than one of these, bu
-	#                         # different names for each port   
-	#                         # e.g. p1, p2, motor, servo1 etc.  
-	  
-	# p.start(90)             # start the PWM on 50 percent duty cycle  
-	#                         # duty cycle value can be 0.0 to 100.0%, floats are OK 
+	pwms = {
+		"l" : 18,
+		"r" : 16
+	}
 
+
+	GPIO.setmode(GPIO.BOARD)
+	# Setup the GPIO pins ready for control
 	for key, pin in pins.items():
-		if key.endswith('f'):
-			GPIO.output(pin, GPIO.HIGH)
+		GPIO.setup(pin,GPIO.OUT)
 
-elif direction == "b":
-	for key, pin in pins.items():
-		if key.endswith('b'):
-			GPIO.output(pin, GPIO.HIGH)
-			print ("hello")
+	# Start PWM for the right side of the robot
+	GPIO.setup(pwms["r"], GPIO.OUT)
+	p = GPIO.PWM(pwms["r"], 50)
+	p.start(speed)
 
-elif direction == "l":
-	for key, pin in pins.items():
-		if key[1] == 'l' and key.endswith('b'):
-			GPIO.output(pin, GPIO.HIGH)
-		if key[1] == 'r' and key.endswith('f'):
-			GPIO.output(pin, GPIO.HIGH)
-else:
-	# If invalid direction throw error
-	raise ValueError("Invalid direction specified", direction)
+	# Start PWM for the left side of the robot
+	GPIO.setup(pwms["l"], GPIO.OUT)
+	p2 = GPIO.PWM(pwms["l"], 50)
+	p2.start(speed)
 
-try:
-	sleep(float(distance))
-except:
+	# Based on direction set the required direction pin to high
+	if direction == "f":
+		for key, pin in pins.items():
+			if key.endswith('f'):
+				GPIO.output(pin, GPIO.HIGH)
+
+	elif direction == "b":
+		for key, pin in pins.items():
+			if key.endswith('b'):
+				GPIO.output(pin, GPIO.HIGH)
+				
+	elif direction == "l":
+		for key, pin in pins.items():
+			if key[1] == 'l' and key.endswith('b'):
+				GPIO.output(pin, GPIO.HIGH)
+			if key[1] == 'r' and key.endswith('f'):
+				GPIO.output(pin, GPIO.HIGH)
+
+	elif direction == "r":
+		for key, pin in pins.items():
+			if key[1] == 'r' and key.endswith('b'):
+				GPIO.output(pin, GPIO.HIGH)
+			if key[1] == 'l' and key.endswith('f'):
+				GPIO.output(pin, GPIO.HIGH)
+
+	else:
+		# If invalid direction throw error
+		raise ValueError("Invalid direction specified", direction)
+
+	try:
+		sleep(float(distance))
+	except:
+		# Set all GPIO pins to low to kill any movement
+		for key, pin in pins.items():
+			GPIO.output(pins, GPIO.LOW)
+
+		# If invalid distance throw error
+		raise ValueError("Invalid Distance Specified", distance)
+		# Release hold on GPIO pins
+		GPIO.cleanup()
+
 	# Set all GPIO pins to low to kill any movement
 	for key, pin in pins.items():
-		GPIO.output(pins, GPIO.LOW)
+		GPIO.output(pin, GPIO.LOW)
 
-	# If invalid distance throw error
-	raise ValueError("Invalid Distance Specified", distance)
+	p.stop()
+	p2.stop()
 	# Release hold on GPIO pins
 	GPIO.cleanup()
-
-# Set all GPIO pins to low to kill any movement
-for key, pin in pins.items():
-	GPIO.output(pin, GPIO.LOW)
-
-#p.stop()
-# Release hold on GPIO pins
-GPIO.cleanup()
